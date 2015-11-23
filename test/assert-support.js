fa8c24dca67fb4fde538dc836688b392615a0f72
James Talmage <james@talmage.io>
2015-11-23 04:12:06 -0500

    wip - mergePlugin

--- test/assert-support.js
+++ test/assert-support.js
@@ -47,3 +47,139 @@ test('createPlugin - calls the function with options', function (t) {
 		}
 	});
 });
+
+test('fixPattern - changes the object name', function (t) {
+	t.equal(
+		support.fixPattern('assert.strictEqual(actual, expected, [message])'),
+		't.strictEqual(actual, expected, [message])'
+	);
+	t.end();
+});
+
+test('fixPattern - throws on an invalid patter', function (t) {
+	t.throws(function () {
+		support.fixPattern('this is not a pattern!');
+	});
+	t.end();
+});
+
+function stub(name, log) {
+	return function () {
+		log.push([name, Array.prototype.slice.call(arguments)]);
+	};
+}
+
+test('mergePlugin - adds methods described by patterns', function (t) {
+	var target = {};
+	var patterns = [];
+	var log = [];
+
+	support.mergePlugin({
+		object: {ok: stub('ok', log)},
+		patterns: ['assert.ok(actual, expected, [message])']
+	}, target, patterns);
+
+	t.equal('function', typeof target.ok);
+	t.same(patterns, ['t.ok(actual, expected, [message])']);
+	t.same(log, []);
+	target.ok('a', 'b');
+	t.same(log, [
+		['ok', ['a', 'b']]
+	]);
+	t.end();
+});
+
+test('mergePlugin - adds methods declared in methods', function (t) {
+	var target = {};
+	var patterns = [];
+	var log = [];
+
+	support.mergePlugin({
+		object: {ok: stub('ok', log)},
+		methods: ['ok']
+	}, target, patterns);
+
+	t.equal('function', typeof target.ok);
+	t.same(patterns, []);
+	t.same(log, []);
+	target.ok('a', 'b');
+	t.same(log, [
+		['ok', ['a', 'b']]
+	]);
+	t.end();
+});
+
+test('mergePlugin - the first plugin wins (method declared in plugin.patterns)', function (t) {
+	var target = {};
+	var patterns = [];
+	var log = [];
+
+	support.mergePlugin({
+		object: {
+			ok: stub('ok - a', log)
+		},
+		patterns: [
+			'a.ok(actual, expected, [message])'
+		]
+	}, target, patterns);
+
+	support.mergePlugin({
+		object: {
+			ok: stub('ok - b', log),
+			notOk: stub('notOk - b', log)
+		},
+		patterns: [
+			'b.ok(expected, actual)',
+			'b.notOk(actual, expected, [message])'
+		]
+	}, target, patterns);
+
+	t.equal('function', typeof target.ok);
+	t.equal('function', typeof target.notOk);
+	t.same(patterns, [
+		't.ok(actual, expected, [message])',
+		't.notOk(actual, expected, [message])'
+	]);
+	t.same(log, []);
+	target.ok('a', 'b');
+	target.notOk('a', 'b');
+	t.same(log, [
+		['ok - a', ['a', 'b']],
+		['notOk - b', ['a', 'b']]
+	]);
+	t.end();
+});
+
+test('mergePlugin - the first plugin wins (method declared in plugin.methods)', function (t) {
+	var target = {};
+	var patterns = [];
+	var log = [];
+
+	support.mergePlugin({
+		object: {
+			ok: stub('ok - a', log)
+		},
+		methods: ['ok']
+	}, target, patterns);
+
+	support.mergePlugin({
+		object: {
+			ok: stub('ok - b', log),
+			notOk: stub('notOk - b', log)
+		},
+		methods: ['ok', 'notOk']
+	}, target, patterns);
+
+	t.equal('function', typeof target.ok);
+	t.equal('function', typeof target.notOk);
+	t.same(patterns, []);
+	t.same(log, []);
+	target.ok('a', 'b');
+	target.notOk('a', 'b');
+	t.same(log, [
+		['ok - a', ['a', 'b']],
+		['notOk - b', ['a', 'b']]
+	]);
+	t.end();
+});
+
